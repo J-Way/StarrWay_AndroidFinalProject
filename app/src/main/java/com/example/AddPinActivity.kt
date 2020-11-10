fa70c26fc2f944bcf189cad2133bd193ca90c137
Francis Starr <38301914+francisstarr@users.noreply.github.com>
2020-11-10 16:23:08 -0500

    list_view_started
    layout for list view

--- app/src/main/java/com/example/AddPinActivity.kt
+++ app/src/main/java/com/example/AddPinActivity.kt
@@ -2,12 +2,15 @@ package com.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.example.starrway_androidfinalproject.R
+import kotlinx.android.synthetic.main.activity_add_pin.*
 
 class AddPinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_pin)
         setTitle("Add Pin")
+        addPinView.adapter=UserFormAdapter(this, arrayOf("a"))
     }
 }
---
+++ app/src/main/java/com/example/UserFormAdapter.kt
@@ -0,0 +1,17 @@
+package com.example
+
+import android.app.Activity
+import android.text.Layout
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.example.starrway_androidfinalproject.R
+
+class UserFormAdapter(private val context: Activity, private val path:Array<String>)
+    :ArrayAdapter<String>(context,R.layout.form_view,path){
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val inflater=context.layoutInflater
+        val rowView=inflater.inflate(R.layout.form_view,null,true)
+        return rowView
+    }
+}
