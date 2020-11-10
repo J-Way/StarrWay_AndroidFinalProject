d80ebeddc8d648030b1c3661d200304f3aab57da
Francis Starr <38301914+francisstarr@users.noreply.github.com>
2020-11-09 20:55:21 -0500

    AddPin_Activity
    AddPin Activity created

---
+++ app/src/main/java/com/example/AddPinActivity.kt
@@ -0,0 +1,13 @@
+package com.example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.starrway_androidfinalproject.R
+
+class AddPinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_pin)
+        setTitle("Add Pin")
+    }
+}
