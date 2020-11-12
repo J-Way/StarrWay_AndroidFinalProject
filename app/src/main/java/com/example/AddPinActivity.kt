92792d9ee67889417d45a741aabf3221ac9085a0
Francis Starr <38301914+francisstarr@users.noreply.github.com>
2020-11-11 20:23:35 -0500

    button_functionality_on_lv
    Figured out how to set onClick listener for layouts in list views

--- app/src/main/java/com/example/AddPinActivity.kt
+++ app/src/main/java/com/example/AddPinActivity.kt
@@ -2,9 +2,11 @@ package com.example
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
 import com.example.starrway_androidfinalproject.R
 import kotlinx.android.synthetic.main.activity_add_pin.*
+
 
 class AddPinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +14,7 @@ class AddPinActivity : AppCompatActivity() {
         setContentView(R.layout.activity_add_pin)
         setTitle("Add Pin")
         addPinView.adapter=UserFormAdapter(this)
+
     }
+
 }
--- app/src/main/java/com/example/UserFormAdapter.kt
+++ app/src/main/java/com/example/UserFormAdapter.kt
@@ -5,6 +5,9 @@ import android.text.Layout
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import com.example.starrway_androidfinalproject.R
 
 class UserFormAdapter(private val context: Activity)
@@ -12,6 +15,10 @@ class UserFormAdapter(private val context: Activity)
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         val inflater=context.layoutInflater
         val rowView=inflater.inflate(R.layout.form_view,null,true)
+        val btnCancel=rowView.findViewById(R.id.btnCancel) as Button
+        btnCancel.setOnClickListener {
+            Toast.makeText(context, "You clicked me.", Toast.LENGTH_SHORT).show()
+        }
         return rowView
     }
 }
