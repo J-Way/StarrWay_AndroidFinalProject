492e1a1fc2789fb42d1c8b49a8e6b3005edbafe5
Francis Starr <38301914+francisstarr@users.noreply.github.com>
2020-11-12 11:09:38 -0500

    camera_act_created
    Activity for camera created and navigable through the use form

---
+++ app/src/main/java/com/example/CameraActivity.kt
@@ -0,0 +1,12 @@
+package com.example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.starrway_androidfinalproject.R
+
+class CameraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+    }
+}
--- app/src/main/java/com/example/UserFormAdapter.kt
+++ app/src/main/java/com/example/UserFormAdapter.kt
@@ -1,6 +1,8 @@
 package com.example
 
 import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
 import android.text.Layout
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +10,7 @@ import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat.startActivity
 import com.example.starrway_androidfinalproject.R
 
 class UserFormAdapter(private val context: Activity)
@@ -17,7 +20,15 @@ class UserFormAdapter(private val context: Activity)
         val rowView=inflater.inflate(R.layout.form_view,null,true)
         val btnCancel=rowView.findViewById(R.id.btnCancel) as Button
         btnCancel.setOnClickListener {
-            Toast.makeText(context, "You clicked me.", Toast.LENGTH_SHORT).show()
+            val intent = Intent(context, MapsActivity::class.java)
+            context.startActivity(intent)
+        }
+
+        val btnCamera=rowView.findViewById(R.id.btnCamera) as Button
+
+        btnCamera.setOnClickListener {
+            val intent = Intent(context, CameraActivity::class.java)
+            context.startActivity(intent)
         }
         return rowView
     }
