0647cfcded9bd8f191e082c1fcd774103d60d21e
Francis Starr <38301914+francisstarr@users.noreply.github.com>
2020-11-14 17:16:38 -0500

    camera_fixed
    Camera now functions

--- app/src/main/java/com/example/CameraActivity.kt
+++ app/src/main/java/com/example/CameraActivity.kt
@@ -16,6 +16,7 @@ import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.R
+import kotlinx.android.synthetic.main.activity_camera.*
 import java.io.File
 import java.lang.Exception
 import java.text.SimpleDateFormat
@@ -57,27 +58,29 @@ class CameraActivity : AppCompatActivity() {
             mediaDir else filesDir
     }
     private fun startCamera(){
-        val imageCapture=imageCapture?: return
-        val photoFile=File(outputDirectory, SimpleDateFormat(FILENAME_FORMAT, Locale.US).format(System.currentTimeMillis())+".jpg")
-        //Create output options which contains files + metadata
-        val outputOptions=ImageCapture.OutputFileOptions.Builder(photoFile).build()
-
-        //set up image capture listener, which is triggerred after photo has been taken
-        imageCapture.takePicture(outputOptions,
-            ContextCompat.getMainExecutor(this), object: ImageCapture.OnImageSavedCallback{
-                override fun onError(exc: ImageCaptureException) {
-                    Log.e(TAG,"Photo capture failed ${exc.message}", exc)
-                }
-
-
-                override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
-                    val savedUri= Uri.fromFile(photoFile)
-                    val msg="Photo capture succeeded "+ savedUri.toString()
-                    Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-                    Log.d(TAG,savedUri.toString())
-                }
+        //Used to bind the lifecycle of camera to the lifecycle owner
+        val cameraProviderFuture= ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider: ProcessCameraProvider =cameraProviderFuture.get()
+            val preview= Preview.Builder().build().also{
+                it.setSurfaceProvider(viewFinder.createSurfaceProvider())
             }
-        )
+            imageCapture=ImageCapture.Builder().build()
+
+
+            //Select back camera as default
+            val cameraSelector= CameraSelector.DEFAULT_BACK_CAMERA
+            try{
+                //unbine use cases before rebuilding
+                cameraProvider.unbindAll()
+
+                //Bind use cases to camera
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+            }
+            catch(exc: Exception){
+                Log.e(TAG,"Use case binding failed", exc)
+            }
+        },ContextCompat.getMainExecutor(this))
     }
     fun shootPhoto(view: View){
         val imageCapture=imageCapture?: return
