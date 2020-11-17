f76ecd245169cdb2826feee4dcbe889e545a098f
Francis Starr <38301914+francisstarr@users.noreply.github.com>
2020-11-16 20:35:01 -0500

    Listview_removed_entirely
    ListView strategy for user form deemed unviabled. Content of listview were instead pasted in directly

--- app/src/main/java/com/example/AddPinActivity.kt
+++ app/src/main/java/com/example/AddPinActivity.kt
@@ -1,15 +1,15 @@
 package com.example
 
 import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.widget.*
 import com.example.R
 import kotlinx.android.synthetic.main.activity_add_pin.*
+import java.io.File
 
 
 class AddPinActivity : AppCompatActivity() {
@@ -17,10 +17,31 @@ class AddPinActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_pin)
         setTitle("Add Pin")
-        addPinView.adapter=UserFormAdapter(this, MapsActivity.activePin)
 
-        //val etTitle=addPinView.findViewById(R.id.etTitle) as EditText
+        etDate.setText( MapsActivity.activePin.date)
+        etDescription.setText(MapsActivity.activePin.description)
+        etTitle.setText( MapsActivity.activePin.title)
+        tvPhotoPath.setText(MapsActivity.activePin.photoPath)
 
+        val minLength=7
+        if (tvPhotoPath.text.toString().length>=minLength){
+            var imgFile= File(tvPhotoPath.text.toString().substring(minLength))
+            if (imgFile.exists()){
+                var b: Bitmap = BitmapFactory.decodeFile(imgFile.toString())
+                imgPath.setImageBitmap(b)
+            }
+        }
+        btnCancel.setOnClickListener {
+            MapsActivity.activePin=Pin()
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnCamera.setOnClickListener {
+            MapsActivity.activePin=Pin(etTitle.text.toString(), etDescription.text.toString(), etDate.text.toString(), tvPhotoPath.text.toString())
+            val intent = Intent(this, CameraActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
